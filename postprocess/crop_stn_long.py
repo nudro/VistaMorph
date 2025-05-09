a29af108e5b12a72fb9f81a34f4ed1e9b53eacae
Catherine Ordun <catherineordun@example.com>
2025-05-09 11:05:33 -0400

    Add crop_stn_long.py for handling 1280x256 images

---
+++ postprocess/crop_stn_long.py
@@ -0,0 +1,68 @@
+import PIL
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+import sys
+import os
+import argparse
+
+"""
+Crops a 1280 x 256 images of stacked images from the test phase:
+real_A, fake_B, real_B and puts them into respective directories. Run
+this before evaluation.py
+
+STN experiment outputs 1280x256 stack of:
+real_A, fake_A, reg_B, fake_A_from_reg_B, real_B
+
+assumes a total of 5 images from the test script
+
+Just want real_A and reg_B and real_B
+"""
+
+def crop_it(infile_path, RA_out, RegB_out, RB_out):
+    dirs = os.listdir(infile_path)
+    counter = 0
+    for item in dirs:
+        fullpath = os.path.join(infile_path, item)
+        if os.path.isfile(fullpath):
+            counter += 1
+            im = Image.open(fullpath) # open the source image
+            f, e = os.path.splitext(fullpath) # file and its extension like a1, .png
+            
+            # For 1280x256 images, we need to crop horizontally
+            # Each image is 256x256, so we need to adjust the x coordinates
+            real_A = im.crop((0, 0, 256, 256))
+            real_B = im.crop((256, 0, 512, 256))
+            fake_B = im.crop((512, 0, 768, 256))
+            fake_wB = im.crop((768, 0, 1024, 256))
+            reg_B = im.crop((1024, 0, 1280, 256))
+
+            save_rA_fname = os.path.join(RA_out, os.path.basename(f) + '.png')
+            save_regB_fname = os.path.join(RegB_out, os.path.basename(f) + '.png')
+            save_rB_fname = os.path.join(RB_out, os.path.basename(f) + '.png')
+
+            real_A.save(save_rA_fname, quality=100)
+            reg_B.save(save_regB_fname, quality=100)
+            real_B.save(save_rB_fname, quality=100)
+            print(counter)
+
+def main(inpath, RA_out, RegB_out, RB_out):
+    crop_it(infile_path=inpath, RA_out=RA_out, RegB_out=RegB_out, RB_out=RB_out)
+
+### MAIN ###
+if __name__ == '__main__':
+    # use crop.sh to declare paths
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--inpath", type=str, default="none", help="path to test results original images")
+    parser.add_argument("--RA_out", type=str, default="none", help="path to real_A visible dir")
+    parser.add_argument("--RegB_out", type=str, default="none", help="path reg_B thermal dir")
+    parser.add_argument("--RB_out", type=str, default="none", help="path to real_B dir")
+    parser.add_argument("--experiment", type=str, default="none", help="experiment_name")
+    opt = parser.parse_args()
+    print(opt)
+
+    os.makedirs("experiments/%s/real_B" % opt.experiment, exist_ok=True)
+    os.makedirs("experiments/%s/reg_B" % opt.experiment, exist_ok=True)
+    os.makedirs("experiments/%s/real_A" % opt.experiment, exist_ok=True)
+
+    main(opt.inpath, opt.RA_out, opt.RegB_out, opt.RB_out) 
